48dc6464659a87835ec34589c80cde08c4debf59
fazlearabbi16 <fazlearabbi07@gmail.com>
2024-02-29 06:53:36 -0800

    Code partion is done

--- index.js
+++ index.js
@@ -4,10 +4,8 @@
  Author: Fazle Rabbi
  Data : 28 feb 2024
 */ 
-
 const http = require('http');
-const url = require('url');
-const {StringDecoder}= require('string_decoder');
+const { hendleReqRes } = require('./helpers/handleReqRes');
 
 // app object - module scaffolding
 const app = {};
@@ -22,28 +20,5 @@ app.createServer = () =>{
         console.log('server is on at port '+app.config.port);
     });
 }
-
-app.handleReqRes = (req,res) =>{
-    // get the url and parse 
-    const parseUrl = url.parse(req.url, true);
-    const pathName = parseUrl.pathname;
-    // Use a regular expression to remove unwanted characters
-    const formatedPath = pathName.replace(/^\/+|\/+$/g, ''); 
-    const method = req.method.toLowerCase();  
-    const queryStringObject = parseUrl.query;
-    const metaDataObject = req.headers;
-    const decoder = new StringDecoder('utf-8');
-    let realData = '';
-    req.on('data',(buffer)=>{
-       realData += decoder.write(buffer);
-    });
-    req.on('end',()=>{
-        realData += decoder.end();
-        console.log(realData);
-        res.end('Real data end');
-     });
-    console.log(metaDataObject);
-    res.end('server run at port '+app.config.port);
-}
-
+app.handleReqRes = hendleReqRes;
 app.createServer();
