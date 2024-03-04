b81eb2e1404581e5a9f931165a8f3e1430fdb67f
fazlearabbi16 <fazlearabbi07@gmail.com>
2024-03-03 19:15:10 -0800

    Delete file successfully

--- index.js
+++ index.js
@@ -12,12 +12,20 @@ const data = require('./lib/data');
 // app object - module scaffolding
 const app = {};
 // data create in lib/data.js folder
-data.updateData('test','newFile',{'name':'John Doe','language':'English'},(err)=>{
-    console.log(err);
-});
-// data read in lib/data.js folder
+// data.createData('test','newFile',{'name':'John Doe','language':'English'},(err)=>{
+//     console.log(err);
+// });
+// data read file
 // data.readData('test','newFile',(err,data)=>{
 //     console.log(err,data);
+// });
+//update file
+// data.updateData('test','newFile',{'name':'Alexa grace','language':'Italian'},(err)=>{
+//         console.log(err);
+//     });
+// delete file 
+// data.deleteData('test','newFile',(err)=>{
+//     console.log(err);
 // });
 app.createServer = () =>{
     const server = http.createServer(app.handleReqRes);
